fed3b2a79570f127f2eb3928a72c1be56f61ee25
venhaserjava <venhaserjava@gmail.com>
2025-04-26 14:45:06 -0400

    Feat: Unidade Service and UnidadeEndereco

---
+++ src/main/java/com/br/unidade/services/UnidadeService.java
@@ -0,0 +1,29 @@
+package com.br.unidade.services;
+
+import com.br.cidade.repositories.CidadeRepository;
+import com.br.endereco.repositories.EnderecoRepository;
+import com.br.unidade.mappers.UnidadeMapper;
+import com.br.unidade.repositories.UnidadeRepository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class UnidadeService {
+/*
+    @Inject
+    UnidadeRepository unidadeRepository;
+
+    @Inject
+    CidadeRepository cidadeRepository;
+
+    @Inject
+    EnderecoRepository enderecoRepository;
+
+    @Inject
+    UnidadeMapper unidadeMapper;
+
+    @Inject
+*/
+
+}
